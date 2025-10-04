e8d6ffcb6c21a46e8ed1201177260b5ff1d1cfed
Qipeax <qipeax@gmail.com>
2025-10-04 16:18:49 +0300

    idk some changes

--- src/components/Head.jsx
+++ src/components/Head.jsx
@@ -1,4 +1,3 @@
-
 import { useLocation, useNavigate } from "react-router-dom";
 
 function Head() {
@@ -6,8 +5,8 @@ function Head() {
   const location = useLocation();
 
   const goToHome = () => navigate("/");
-  const goToCinemas = () => navigate("/Кинотеатры");
-  const goToEvents = () => navigate("/events");
+  const goToProjects = () => navigate("/Projects");
+  const goToEvents = () => navigate("/Events");
 
   const isActive = (path) => location.pathname === path;
 
@@ -17,27 +16,37 @@ function Head() {
         <h1 className="logo">Квикенд</h1>
         <ul>
           <li>
-            <button 
-              onClick={goToHome} 
+            <button
+              onClick={goToHome}
               className={`headerButton ${isActive("/") ? "active" : ""}`}
             >
-              Фильмы
+              Главная страница
             </button>
           </li>
           <li>
-            <button 
-              onClick={goToCinemas} 
-              className={`headerButton ${isActive("/Кинотеатры") ? "active" : ""}`}
+            <button
+              onClick={goToProjects}
+              className={`headerButton ${
+                isActive("/Projects") ? "active" : ""
+              }`}
             >
-              Кинотеатры
+              Проэкты
             </button>
           </li>
           <li>
-            <button 
-              onClick={goToEvents} 
+            <button
+              onClick={goToEvents}
               className={`headerButton ${isActive("/events") ? "active" : ""}`}
             >
-              События
+              Задачи
+            </button>
+          </li>
+          <li>
+            <button
+              onClick={goToEvents}
+              className={`headerButton ${isActive("/events") ? "active" : ""}`}
+            >
+              Создать
             </button>
           </li>
         </ul>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,11 @@
 import Head from "../components/Head.jsx";
 
-
 function Home() {
   return (
     <>
-    
       <header>
         <Head />
       </header>
-
-
     </>
   );
 }
